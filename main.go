3cdffec6d7dd25b5038a5762aaa40c7f70f9f181
Sean <41174337+ouyangke584@users.noreply.github.com>
2026-06-13 13:16:42 +0800

    fix 401

--- main.go
+++ main.go
@@ -1344,7 +1344,7 @@ func getToken() (string, error) {
 		return "", err
 	}
 
-	regex = regexp.MustCompile([A-Za-z0-9_-]{16,}\.[A-Za-z0-9_-]{16,}\.[A-Za-z0-9_-]{16,})
+	regex = regexp.MustCompile(`eyJ0([^"]*)`)
 	token := regex.FindString(string(body))
 
 	return token, nil
